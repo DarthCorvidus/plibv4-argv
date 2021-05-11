78308f22e83dc0c9b5992a87e4f29bc77e1f07f8
DarthCorvidus <github@vm01.telton.de>
2021-05-11 21:13:05 +0200

    Argument representing an ISO date.

---
+++ src/ArgDate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright (c) 2019, Claus-Christoph Küthe
+ * @author Claus-Christoph Küthe <floss@vm01.telton.de>
+ * @license LGPL
+ */
+
+class ArgDate extends ArgString {
+	public function __construct() {
+		parent::__construct();
+		$this->setValidate(new ValidateDate(ValidateDate::ISO));
+	}
+}
